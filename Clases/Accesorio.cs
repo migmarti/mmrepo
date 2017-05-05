f987dae33c514cde2c595e35d65ca89e69941a32
migmarti <mmart96@hotmail.com>
2017-05-05 13:17:59 -0700

    https://github.com/migmarti/mmrepo.git

---
+++ Clases/Accesorio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Argus_CRM.Clases
+{
+    public class Accesorio : Material
+    {
+
+        public Accesorio(): base()
+        {
+
+        }
+        public Accesorio(String nombre,String marca, String modelo, String ficha, float costo): base(nombre,marca,modelo,ficha,costo)
+        {
+            
+        }
+
+    }
+}
